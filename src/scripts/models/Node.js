808fe67039ad588a685d3076cbb1728ddae8b682
Lucas Formaggine <53341833+lucasformaggine@users.noreply.github.com>
2024-10-13 14:23:53 -0300

    Refact: creating 'getEdge' function, cleaning up Node facade and fixing edgehandles duplicate edge creation (#34)
    * Prevent creating duplicate edge between two nodes using edgehandles
    
    * Creating getEdge(node1, node2) and refactoring Node facade

--- src/scripts/models/Node.js
+++ src/scripts/models/Node.js
@@ -1,11 +1,10 @@
 export class Node {
-    constructor({ id, weight = 1, tag = '', isDirected }) {
+    constructor({ id, weight = 1, tag = '' }) {
         this.id = id;
         this.weight = weight;
         this.tag = tag;
         this.outgoingEdges = [];
         this.incomingEdges = [];
-        this.isDirected = isDirected;
     }
 
     addOutgoingEdge(edge) {
@@ -14,27 +13,5 @@ export class Node {
 
     addIncomingEdge(edge) {
         this.incomingEdges.push(edge);
-    }
-
-    getNeighbors() {
-        const neighbors = [];
-
-        const addNeighbor = (node) => {
-            if (!neighbors.includes(node)) {
-                neighbors.push(node);
-            }
-        };
-
-        this.outgoingEdges.forEach(edge => addNeighbor(edge.targetNode));
-
-        if (!this.isDirected) {
-            this.incomingEdges.forEach(edge => addNeighbor(edge.sourceNode));
-        }
-
-        return neighbors;
-    }
-
-    getEdges() {
-        return [...this.incomingEdges, ...this.outgoingEdges];
     }
 }
