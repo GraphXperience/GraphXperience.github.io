c108eb951a20d07db8808a3a9c00839fb6ad2a3b
Lucas <lucasformaggine@hotmail.com>
2025-02-01 14:46:42 -0300

    Fixing bipartite and complete bipartite partition definitions

--- src/scripts/predefined-graphs/bipartite-graph.js
+++ src/scripts/predefined-graphs/bipartite-graph.js
@@ -1,22 +1,25 @@
 import { getRandomUuid } from '../utils';
 
 function createBipartiteGraph(cy, ur, nodeCount, nodeCount2) {
-    let nodes = [];
-    let nodes2 = [];
+    let largerPartitionNodes = [];
+    let smallerPartitionNodes = [];
 
-    for (let i = 0; i < nodeCount; i++) {
-        nodes.push({ data: { id: getRandomUuid() } });
+    let largerPartitionCount = Math.max(nodeCount, nodeCount2);
+    let smallerPartitionCount = Math.min(nodeCount, nodeCount2);
+
+    for (let i = 0; i < largerPartitionCount; i++) {
+        largerPartitionNodes.push({ data: { id: getRandomUuid() } });
     }
 
-    ur.do('add', nodes);
+    ur.do('add', largerPartitionNodes);
 
-    for (let i = 0; i < nodeCount2; i++) {
-        nodes2.push({ data: { id: getRandomUuid() } });
+    for (let i = 0; i < smallerPartitionCount; i++) {
+        smallerPartitionNodes.push({ data: { id: getRandomUuid() } });
     }
 
-    ur.do('add', nodes2);
+    ur.do('add', smallerPartitionNodes);
 
-    cy.layout({ name: 'grid', rows: 2, columns: Math.max(nodeCount, nodeCount2)}).run();
+    cy.layout({ name: 'grid', rows: 2, columns: largerPartitionCount}).run();
 }
 
 export {
