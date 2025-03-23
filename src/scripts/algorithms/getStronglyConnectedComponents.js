c3b154124c70af39ccf204cf1e7a0cbce32cdf79
Lucas <lucasformaggine@hotmail.com>
2025-03-23 20:44:19 -0300

    Adding more detaisl to algorithm print messages for better understanding

--- src/scripts/algorithms/getStronglyConnectedComponents.js
+++ src/scripts/algorithms/getStronglyConnectedComponents.js
@@ -12,6 +12,12 @@ function getStronglyConnectedComponents(graph) {
     let i = 0;
     const colors = ['red', 'green', 'blue', 'yellow', 'black', 'aqua', 'orange'];
 
+    let idToTag = new Map();
+
+    for (const node of graph.nodes) {
+        idToTag.set(node.id, node.tag);
+    }
+
     for (const node of graph.nodes) {
         if (!indexMap.has(node.id)) {
             tarjanDfs(node);
@@ -21,6 +27,8 @@ function getStronglyConnectedComponents(graph) {
     return actions;
 
     function tarjanDfs(node) {
+        actions.push({ type: 'print', message: 'Visitando o nó ' + node.tag });
+
         indexMap.set(node.id, index);
         lowLinkMap.set(node.id, index);
 
@@ -51,6 +59,8 @@ function getStronglyConnectedComponents(graph) {
             component.forEach(nodeId => {
                 actions.push({ elementId: nodeId, type: 'animate', color: componentColor });
             });
+
+            actions.push({ type: 'print', message: 'Encontrado o componente fortemente conexo: ' + component.map(id => idToTag.get(id)).join(', ') });
         }
     }
 }
